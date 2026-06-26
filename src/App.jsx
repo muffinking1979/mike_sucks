f56c646350d73d1844d80e1ddd9ce9c6b46cd0f0
muffinking1979 <m.zeigler417@gmail.com>
2026-06-26 15:53:57 -0400

    Stretch words to fill viewport height; scroll to see .com and photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
+import { useRef, useEffect } from 'react'
 import './App.css'
 import mikeImg from './assets/MSS.jpeg'
 
 function Word({ text }) {
-  return (
-    <p>
-      {text.split('').map((ch, i) => <span key={i}>{ch}</span>)}
-    </p>
-  )
+  const ref = useRef()
+
+  useEffect(() => {
+    const el = ref.current
+    const scale = el.parentElement.clientWidth / el.scrollWidth
+    el.style.transform = `scaleX(${scale})`
+  }, [])
+
+  return <p ref={ref}>{text}</p>
 }
 
 export default function App() {
   return (
     <div id="container">
-      <Word text="Mike" />
-      <Word text="Stamm" />
-      <Word text="Sucks" />
-      <Word text=".com" />
+      <div id="words">
+        <Word text="Mike" />
+        <Word text="Stamm" />
+        <Word text="Sucks" />
+      </div>
+      <p id="dotcom">.com</p>
       <img src={mikeImg} alt="Mike Stamm" id="mike-img" />
     </div>
   )
